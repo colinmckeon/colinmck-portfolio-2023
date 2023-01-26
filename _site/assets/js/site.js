12d07f992a9f8a6f85f30b672a8370556438c3c2
Colin McKeon <14mckeon@gmail.com>
2023-01-26 18:03:32 -0500

    Large design updates

--- _site/assets/js/site.js
+++ _site/assets/js/site.js
@@ -24,3 +24,65 @@ $('nav a').click(function(event) {
     }, 500);
     event.preventDefault();
 });
+
+
+
+document.addEventListener('click', function (event) {
+  console.log(event.target.parentElement.parentElement);
+  if (event.target.parentElement.parentElement.matches('.gallery-image__preview')) {
+    event.target.parentElement.parentElement.classList.remove('gallery-image__preview');
+    event.target.parentElement.parentElement.scrollIntoView();
+  } else if (event.target.matches('.gallery-image__media')) {
+    // Remove any existing preview classes
+    var previewing = document.getElementsByClassName('gallery-image__preview');
+    for (el of previewing) {
+      el.classList.remove('gallery-image__preview');
+    }
+        // Preview the clicked figure
+    event.target.parentElement.parentElement.classList.add('gallery-image__preview');
+    event.target.parentElement.parentElement.scrollIntoView();
+    }
+}, false);
+
+
+
+
+function openNav() {
+  var sideBar = document.getElementById("sidebar");
+  if(sideBar.style.display == "block") {
+    sideBar.style.display = "none";
+  }
+  else {
+    sideBar.style.display = "block";
+  }
+}
+
+function closeNav() {
+  var sideBar = document.getElementById("sidebar");
+  if(sideBar.style.display == "none") {
+    sideBar.style.display = "block";
+  }
+  else {
+    sideBar.style.display = "none";
+  }
+}
+
+
+
+var main_body = document.getElementById("main-body");
+
+main_body.addEventListener('mouseup', function(event){
+    var sideBar = document.getElementById('sidebar');
+    if (event.target != sideBar && event.target.parentNode != sideBar){
+        sideBar.style.display = 'none';
+    }
+});
+
+var header = document.getElementById("header")
+header.addEventListener('mouseup', function(event){
+    var sideBar = document.getElementById('sidebar');
+    if (event.target != sideBar && event.target.parentNode != sideBar){
+        sideBar.style.display = 'none';
+    }
+});
+
